cf63a828c4de5943f211c04c200bec787336d7ec
Met4lizer <steax@mail.ru>
2015-06-04 16:13:10 +1000

    JavaRush lessons

---
+++ src/com/javarush/test/level12/lesson06/task04/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.test.level12.lesson06.task04;
+
+/* Класс Cow от Animal
+Унаследуй класс Cow от Animal.
+Реализуй все недостающие методы в классе Cow.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static class Animal
+    {
+        public String getName() {
+            return null;
+        }
+    }
+
+    public static class Cow extends Animal
+    {
+    }
+
+}
