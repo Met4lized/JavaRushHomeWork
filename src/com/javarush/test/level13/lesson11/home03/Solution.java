391bf85cd0be797478959a84edb421eb44a593ea
Met4lizer <steax@mail.ru>
2015-06-05 14:29:45 +1000

    JavaRush lessons

---
+++ src/com/javarush/test/level13/lesson11/home03/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.test.level13.lesson11.home03;
+
+/* Чтение файла
+1. Считать с консоли имя файла.
+2. Вывести в консоль(на экран) содержимое файла.
+3. Не забыть закрыть файл и поток.
+*/
+
+import java.io.*;
+
+public class Solution
+{
+    public String toString(String data){
+    return data;
+}
+    public static void main(String[] args) throws IOException {
+        //add your code here
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        InputStream inStream = new FileInputStream(reader.readLine());
+        while (inStream.available()>0){
+            System.out.print((char)inStream.read());
+
+        }
+
+        System.out.println();
+        inStream.close();
+
+    }
+}
