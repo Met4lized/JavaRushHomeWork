a1575d1d14c1045d16a18ffd94833d2cc5891cf3
Met4lizer <steax@mail.ru>
2015-06-26 17:52:22 +1000

    JavaRush lessons

---
+++ src/com/javarush/test/Tests/DataStructure.java
@@ -0,0 +1,53 @@
+package com.javarush.test.Tests;
+
+import java.util.Iterator;
+
+public class DataStructure {
+
+    private final static int SIZE = 15;
+    private int[] arrayOfInts = new int[SIZE];
+
+    public DataStructure() {
+        for (int i = 0; i < SIZE; i++) {
+            arrayOfInts[i] = i;
+        }
+    }
+
+    public void printEven() {
+
+        Iterator iterator = this.new EvenIterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+    }
+
+    private class EvenIterator implements java.util.Iterator<Integer> {
+
+        private int nextIndex = 0;
+
+        public boolean hasNext() {
+
+            return (nextIndex <= SIZE - 1);
+        }
+
+        public Integer next() {
+
+            Integer retValue = Integer.valueOf(arrayOfInts[nextIndex]);
+
+            nextIndex += 2;
+            return retValue;
+        }
+
+        @Override
+        public void remove() {
+
+        }
+    }
+
+    public static void main(String s[]) {
+
+        DataStructure ds = new DataStructure();
+        ds.printEven();
+    }
+}
---
+++ src/com/javarush/test/level23/lesson08/home01/vo/User.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level23.lesson08.home01.vo;
+
+/**
+ * Created by ausholohov on 26.06.2015.
+ */
+public class User extends NamedItem {
+}
